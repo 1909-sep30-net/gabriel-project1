8d5ab39e0a0fa30f130400581413cbb779309b8d
yetinight <gang@ucsd.edu>
2019-10-17 23:09:14 -0500

    redid mapping logic, tried to incorporate dictionary

--- DoapSoapRevamped/DoapSoap.BusinessLogic/Models/Customer.cs
+++ DoapSoapRevamped/DoapSoap.BusinessLogic/Models/Customer.cs
@@ -6,6 +6,8 @@ namespace DoapSoap.BusinessLogic.Models
 {
     public class Customer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
---
+++ DoapSoapRevamped/DoapSoap.DataAccess/Mapper.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DoapSoap.DataAccess.Entities;
+
+namespace DoapSoap.DataAccess
+{
+    public static class Mapper
+    {
+        public static BusinessLogic.Models.Location MapLocation(Entities.Locations location)
+        {
+            return new BusinessLogic.Models.Location
+            {
+                ID = location.LocationId,
+                Name = location.Name,
+                OrderHistory = location.Orders.Select(MapOrder).ToHashSet(),
+                Inventory = location.InventoryItems.ToDictionary(o=>MapProduct(o.Product),o=>o.Quantity)
+            };
+        }
+        public static Entities.Locations MapLocation(BusinessLogic.Models.Location location)
+        {
+            return new Entities.Locations
+            {
+                LocationId = location.ID,
+                Name = location.Name
+            };
+        }
+
+        public static BusinessLogic.Models.Customer MapCustomer(Entities.Customers customer)
+        {
+            return new BusinessLogic.Models.Customer
+            {
+                ID = customer.CustomerId,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Phone = customer.Phone,
+                OrderHistory = customer.Orders.Select(MapOrder).ToHashSet()
+            };
+        }
+
+        public static Entities.Customers MapCustomer(BusinessLogic.Models.Customer customer)
+        {
+            return new Entities.Customers
+            {
+                CustomerId = customer.ID,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Phone = customer.Phone
+            };
+        }
+
+        public static BusinessLogic.Models.Order MapOrder(Entities.Orders order)
+        {
+            return new BusinessLogic.Models.Order
+            {
+                ID = order.OrderId,
+                TimePlaced = order.TimeConfirmed,
+                ProductList = order.OrderItems.ToDictionary(o => MapProduct(o.Product), o => o.Quantity)
+                
+            };
+        }
+        public static Entities.Orders MapOrder(BusinessLogic.Models.Order order)
+        {
+            return new Entities.Orders
+            {
+                OrderId = order.ID,
+                LocationId = order.Location.ID,
+                CustomerId = order.Customer.ID,
+                TimeConfirmed = order.TimePlaced,
+                OrderItems = MapOrderItems(order.ProductList)
+            };
+        }
+        public static HashSet<Entities.OrderItems> MapOrderItems(Dictionary<BusinessLogic.Models.Product,int> pl)
+        {
+            HashSet<Entities.OrderItems> OIList = new HashSet<Entities.OrderItems>();
+            foreach (KeyValuePair<BusinessLogic.Models.Product,int> key in pl)
+            {
+                OIList.Add(new Entities.OrderItems
+                {
+                    Product = MapProduct(key.Key),
+                    Quantity = key.Value
+                });
+            }
+            return OIList;
+        }
+        private static HashSet<Entities.InventoryItems> MapInventoryItems(InventoryItems arg1, int arg2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BusinessLogic.Models.Product MapProduct(Entities.Products product)
+        {
+            return new BusinessLogic.Models.Product
+            {
+                ID = product.ProductId,
+                Name = product.Name,
+                Price = product.Price,
+                Color = MapColor(product.Color)
+            };
+        }
+
+        public static Entities.Products MapProduct(BusinessLogic.Models.Product product)
+        {
+            //TODO: check if we actually need the rest of the values when adding orders back into db
+            return new Entities.Products
+            {
+                ProductId = product.ID,
+            };
+        }
+
+        public static BusinessLogic.Models.Color MapColor (Entities.Colors color)
+        {
+            return new BusinessLogic.Models.Color
+            {
+                ID = color.ColorId,
+                Name = color.Name
+            };
+        }
+    }
+}
